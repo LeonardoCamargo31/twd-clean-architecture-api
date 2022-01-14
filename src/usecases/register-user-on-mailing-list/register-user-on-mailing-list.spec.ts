fcd0d4ea701313a36ab7216778270cdf95493229
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-01-14 20:33:04 -0300

    feat: user repository interface

---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,14 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    // const users: UserData[] = []
+    // const repository: UserRepository = new InMemoryUserRepository(users)
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+    // const name = 'any_name'
+    // const email = 'any@email.com'
+    // await useCase.registerUserOnMailingList({ name, email })
+    // const user = await repository.findUserByEmail('any@email.com')
+    // expect(user.name).toBe('any_name')
+  })
+})
