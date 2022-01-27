d9d800c155cd6adf5e839f7df6f1591c6ae3bb27
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-01-27 20:43:53 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,11 +1,19 @@
 import { UserData } from './user-data'
-import { Either, left } from '../shared'
+import { Either, left, right } from '../shared'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { Email } from './email'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { Name } from './name'
 
 export class User {
+  public readonly name:Name
+  public readonly email:Email
+
+  private constructor (name:Name, email:Email) {
+    this.name = name
+    this.email = email
+  }
+
   // return InvalidEmailError or User
   static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
     const nameOrError = Name.create(userData.name)
@@ -16,5 +24,9 @@ export class User {
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
     }
+
+    const name = nameOrError.value
+    const email = emailOrError.value
+    return right(new User(name, email))
   }
 }
