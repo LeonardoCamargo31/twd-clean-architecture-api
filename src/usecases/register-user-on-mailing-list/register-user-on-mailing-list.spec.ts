dd8dbc1736e5e39d1edc1a51db8d88b46cfe7955
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-02-01 19:17:46 -0300

    feat: improve error classes

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,14 +1,43 @@
-// import { UserData } from './user-data'
+import { UserData } from '../../entities/user-data'
+import { UserRepository } from './ports/user-repository'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
 
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
-    // const users: UserData[] = []
-    // const repository: UserRepository = new InMemoryUserRepository(users)
-    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
-    // const name = 'any_name'
-    // const email = 'any@email.com'
-    // await useCase.registerUserOnMailingList({ name, email })
-    // const user = await repository.findUserByEmail('any@email.com')
-    // expect(user.name).toBe('any_name')
+    const users: UserData[] = []
+    const repository: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+    const name = 'any_name'
+    const email = 'any@email.com'
+    await useCase.registerUserOnMailingList({ name, email })
+    const user = await repository.findUserByEmail('any@email.com')
+    expect(user.name).toBe('any_name')
+  })
+
+  test('should not add user with invalid email', async () => {
+    const users: UserData[] = []
+    const repository: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+    const name = 'any_name'
+    const invalidEmail = 'invalid_email'
+    const response = (await useCase.registerUserOnMailingList({ name, email: invalidEmail })).value as Error
+    const user = await repository.findUserByEmail('any@email.com')
+    expect(user).toBeNull()
+    expect(response.name).toEqual('InvalidEmailError')
+    expect(response.message).toEqual('Invalid email: ' + invalidEmail + '.')
+  })
+
+  test('should not add user with invalid name', async () => {
+    const users: UserData[] = []
+    const repository: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repository)
+    const invalidName = ''
+    const email = 'any@email.com'
+    const response = (await useCase.registerUserOnMailingList({ name: invalidName, email })).value as Error
+    const user = await repository.findUserByEmail('any@email.com')
+    expect(user).toBeNull()
+    expect(response.name).toEqual('InvalidNameError')
+    expect(response.message).toEqual('Invalid name: ' + invalidName + '.')
   })
 })
---
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.ts
@@ -0,0 +1,27 @@
+import { UserRepository } from './ports/user-repository'
+import { UserData } from '../../entities/user-data'
+import { Either, left, right } from '../../shared'
+import { User } from '../../entities/user'
+import { InvalidEmailError } from '../../entities/errors/invalid-email-error'
+import { InvalidNameError } from '../../entities/errors/invalid-name-error'
+
+export class RegisterUserOnMailingList {
+  private readonly userRepository:UserRepository
+
+  constructor (userRepository:UserRepository) {
+    this.userRepository = userRepository
+  }
+
+  async registerUserOnMailingList (request: UserData):Promise<Either<InvalidNameError | InvalidEmailError, UserData>> {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> = User.create(request)
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    if (!(await this.userRepository.exists(request))) {
+      await this.userRepository.add(request)
+    }
+
+    return right(request)
+  }
+}
