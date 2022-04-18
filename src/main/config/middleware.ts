1ad6920dca8cd1d4863233b506e6d3ca1cee674f
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-04-18 12:16:39 -0300

    feat: apply cors enabling middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/config/middleware/body-parser'
+import { cors } from '@/main/config/middleware/cors'
 
 export const setupMiddleware = (app:Express):void => {
   app.use(bodyParser)
+  app.use(cors)
 }
