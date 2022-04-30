42b5e74c3580f65b80e5594dfe8088ec3380277c
Leonardo Camargo <leonardo_camargo31@hotmail.com>
2022-04-30 16:37:52 -0300

    refactor: move middlewares, adapters and routes to inside main folder

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -5,7 +5,7 @@ import path from 'path'
 export const setupRoutes = (app:Express):void => {
   const router = Router()
   app.use('/api', router)
-  const pathRoutes = path.join(__dirname, 'routes')
+  const pathRoutes = path.join(__dirname, '..', 'routes')
   readdirSync(pathRoutes)
     .filter(item => !/(test\.ts|index.ts)/g.test(item))
     .map(async file => {
--- src/main/routes/register-route.ts
+++ src/main/routes/register-route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
-import { adapterRoute } from '@/main/config/adapters'
-import { makeRegisterUserController } from '@/main/config/factories/'
+import { adapterRoute } from '@/main/adapters'
+import { makeRegisterUserController } from '@/main/factories'
 
 export default (route:Router):void => {
   route.post('/register', adapterRoute(makeRegisterUserController()))
